1e602437f0527a4c5a13c3d2b522de3a38bdc7f2
Hector Pulido <hector.pulido@platzi.com>
2023-03-19 01:59:35 -0600

    initial commit

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN_DISCORD = os.getenv("TOKEN_DISCORD")
 
-breakpoint()
-
 if __name__ == "__main__":
     bot = LLamaBot()
     bot.run(TOKEN_DISCORD)
