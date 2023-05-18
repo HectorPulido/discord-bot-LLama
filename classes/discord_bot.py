37fe5e27743eb18e8944eb8d1e823f59f4a003c3
Hector Pulido <hector.pulido@platzi.com>
2023-05-18 05:58:59 -0600

    logs added

--- classes/discord_bot.py
+++ classes/discord_bot.py
@@ -1,5 +1,6 @@
 import re
 import os
+import logging
 import asyncio
 import discord
 from discord.ext.commands import Bot
@@ -9,9 +10,9 @@ from classes.llama_model import LlamaModel
 class LLamaBot(Bot):
     def __init__(self):
         model_name = os.getenv("MODEL_NAME")
-        self.model = LlamaModel(model_name=model_name, memory_size=1, temp=0.9)
-
         self.model_lock = False
+
+        self.model = LlamaModel(model_name=model_name, memory_size=1, temp=0.9)
 
         intents = discord.Intents.default()
         intents.message_content = True
@@ -25,6 +26,7 @@ class LLamaBot(Bot):
             return
 
         if "!change_status" in message.content:
+            logging.info("Changing status...")
             new_status = message.content.split("!change_status")[1]
             await self.change_presence(activity=discord.Game(name=new_status))
 
@@ -32,15 +34,19 @@ class LLamaBot(Bot):
             return
 
         while self.model_lock:
+            logging.info("Waiting for model to unlock...")
             await asyncio.sleep(1)
 
         message_text = str(message.content)
         message_text = re.sub(r"<@\d+>", "", message_text).strip()
         message_text = f"{message.author.display_name}: {message_text}"
 
+        logging.info(f"Message received: {message_text}")
+
         async with message.channel.typing():
             self.model_lock = True
             response = await self.model.evaluate(message_text)
+            logging.info(f"Response: {response}")
             await message.reply(response, mention_author=True)
 
         await asyncio.sleep(1)
--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import os
+import logging
 from classes.discord_bot import LLamaBot
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN_DISCORD = os.getenv("TOKEN_DISCORD")
+
+logging.basicConfig(
+    level=logging.INFO,
+    filename="app.log",
+    filemode="w",
+    format="%(asctime)s -%(name)s - %(levelname)s - %(message)s",
+    datefmt="%d-%b-%y %H:%M:%S",
+)
 
 if __name__ == "__main__":
     bot = LLamaBot()
