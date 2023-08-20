8a0deb8da7f35d3bd6c5ab16461b844b49667ce5
Hector Pulido <hector.pulido@platzi.com>
2023-08-20 03:05:22 -0500

    test in memory module

--- source/tests/test_memory.py
+++ source/tests/test_memory.py
@@ -1,7 +1,17 @@
-from source.memory_models.memory_model import MemoryModel
+"""
+Test suite for the memory module
+"""
+
+import os
+from source.memory_models import MemoryModel, MultiChannelMemory
+
+PICKLE_PATH = "test.pkl"
 
 
-def test_memory():
+def test_memory_size():
+    """
+    The memory size should be conscistent
+    """
     memory = MemoryModel(3)
 
     assert memory.get_conversation_length() == 0
@@ -11,7 +21,70 @@ def test_memory():
     memory.append_conversation("Bien y tu?")
 
     assert memory.get_conversation_length() == 3
+    assert len(memory.historial_conversation().split("\n")) == 3
 
     memory.append_conversation("Muy bien")
 
-    assert memory.get_conversation_length() == 3
+    assert memory.get_conversation_length() == 4
+    assert len(memory.historial_conversation().split("\n")) == 3
+
+    memory.clear_conversation()
+    assert memory.get_conversation_length() == 0
+
+
+def test_memory_last_response():
+    """
+    The last response should be conscistent
+    """
+    memory = MemoryModel(3)
+    memory.set_last_response("Hola 1")
+    assert memory.get_last_response() == "Hola 1"
+
+
+def test_multichannel_memory_save():
+    """
+    The multichannel memory should be saved and loaded correctly
+    """
+
+    try:
+        os.remove(PICKLE_PATH)
+    except OSError:
+        pass
+
+    memory = MultiChannelMemory(3, PICKLE_PATH)
+    assert len(memory.memories) == 0
+    memory.get_memory(1).append_conversation("Hola 2")
+    memory.get_memory(1).append_conversation("Como estas? 2")
+    memory.persist_memory()
+
+    assert os.path.exists(PICKLE_PATH)
+
+    memory2 = MultiChannelMemory(3, PICKLE_PATH)
+    assert len(memory2.memories) == 1
+    assert memory2.get_memory(1).get_conversation_length() == 2
+
+    os.remove(PICKLE_PATH)
+
+
+def test_multichannel_clear():
+    """
+    The multichannel memory should be cleared correctly
+    """
+
+    memory = MultiChannelMemory(3)
+    assert len(memory.memories) == 0
+    memory.get_memory(1).append_conversation("Hola 3")
+    memory.get_memory(1).append_conversation("Como estas? 3")
+    memory.get_memory(2).append_conversation("Hola 4")
+    memory.get_memory(2).append_conversation("Como estas? 4")
+
+    assert memory.get_memory(1).get_conversation_length() == 2
+    assert memory.get_memory(2).get_conversation_length() == 2
+
+    memory.clear_channel_memory(1)
+    assert memory.get_memory(1).get_conversation_length() == 0
+    assert memory.get_memory(2).get_conversation_length() == 2
+
+    memory.clear_all_memory()
+    assert memory.get_memory(1).get_conversation_length() == 0
+    assert memory.get_memory(2).get_conversation_length() == 0
